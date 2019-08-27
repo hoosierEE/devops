d088e9101fac11e45ea29f002cc21cb0afeabc4b
hoosieree <ashroyer-admin@129-79-186-253.dhcp-bl.indiana.edu>
2019-08-27 13:54:12 -0400

    maybe cross platform, eh is it worth it?

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,6 @@
 # basically a Dockerfile, but in Python3
-from platform import machine
+# NOTE: Ubuntu only
+import platform
 import subprocess
 import sys
 
@@ -16,55 +17,55 @@ repodir = '$HOME/repo/'
 
 # aliases for deb packages (just the one at the moment)
 JVER   = '807'  # or 807
-mach   = int(machine()=='aarch64')
+mach   = int(platform.machine()=='aarch64')
 jurl   = 'j{}_{}64.deb'.format(JVER, ['amd','arm'][mach])
 jbeta  = 'http://www.jsoftware.com/download/j901/install/j901_'+['linux','raspi'][mach]+'64.tar.gz'
 
 # strings of commands, sent (in order) to subprocess.run()
 TARGETS = {
     'tmux': [
-        apt+'tmux',
+        apt + 'tmux',
     ],
     'emacs': [
-        apt+'emacs25-nox unifont',
+        apt + 'emacs25-nox unifont',
     ],
     'emacs_org_fix': [
         'for dir in "$HOME/.emacs.d/elpa/org-"*/; do find $dir -name "*.elc" -delete; done',
     ],
     'xinu': [
-        apt+'qemu gawk bison flex libz-dev gcc-arm-none-eabi make',
-        gitrec+'git@github.iu.edu:ashroyer/xinu-s19.git '+repodir+'xinu-s19',
+        apt + 'qemu gawk bison flex libz-dev gcc-arm-none-eabi make',
+        gitrec + 'git@github.iu.edu:ashroyer/xinu-s19.git ' + repodir + 'xinu-s19',
     ],
     'k': [
-        apt+'rlwrap make',
-        git+'git@github.com:kevinlawler/kona.git '+repodir+'kona',
-        'cd '+repodir+'kona && make && cd -',
-        'rm -f '+bindir+'k',
-        'ln -s '+repodir+'kona/k '+bindir+'k',
+        apt + 'rlwrap make',
+        git + 'git@github.com:kevinlawler/kona.git ' + repodir + 'kona',
+        'cd ' + repodir + 'kona && make && cd -',
+        'rm -f ' + bindir + 'k',
+        'ln -s ' + repodir + 'kona/k ' + bindir + 'k',
     ],
     'j': [
-        curl+'http://www.jsoftware.com/download/j'+JVER+'/install/'+jurl,
-        dpkg+jurl,
-        'ln -s $(which ijconsole) '+bindir+'j',
-        'rm '+jurl,
+        curl + 'http://www.jsoftware.com/download/j' + JVER + '/install/' + jurl,
+        dpkg + jurl,
+        'ln -s $(which ijconsole) ' + bindir + 'j',
+        'rm ' + jurl,
     ],
     'jbeta': [
-        curl+jbeta,
+        curl + jbeta,
         'tar xzf j901*',
         'mv j901* ~/',
-        'ln -s ~/j901/bin/jconsole '+bindir+'j',
-        'rm ~/j901_'+['linux','raspi'][mach]+'64.tar.gz',
+        'ln -s ~/j901/bin/jconsole ' + bindir + 'j',
+        'rm ~/j901_' + ['linux','raspi'][mach] + '64.tar.gz',
         'rm -r j901',
     ],
-    'e110': [
-        apt+'python3-pip',
+    'myhdl': [
+        apt + 'python3-pip',
         'pip3 install --user myhdl',
     ],
     'tex': [
         'wget -qO- "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh'
     ],
     # 'texlive': [
-    #     apt+'texlive-latex-recommended',
+    #     apt + 'texlive-latex-recommended',
     # ]
 }
 
