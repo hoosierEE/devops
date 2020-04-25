6cb920e664845c96474bf429536292b09e0c305f
hoosieree <ashroyer@gmail.com>
2020-04-25 17:08:35 -0400

    use argparse, cleanup code a bit

--- tlmgr_fix.py
+++ tlmgr_fix.py
@@ -1,47 +1,54 @@
 #!/usr/bin/python3
-# Repeatedly attempt to compile the given .tex file,
-# installing missing package as needed.
-# usage:
-#    tlmgr_fix filename.tex
-import sys, re
+import re
 import subprocess as s
+import sys
 
-def compile_while(filename):
-    x = trycompile(filename)
+def compile_until_no_errors(filename):
+    x = compile_once(filename)
     while x is not None:
         install(find(x))
-        x = trycompile(filename)
+        x = compile_once(filename)
 
-def trycompile(filename):
+def compile_once(filename):
     cmd = f'pdflatex -interaction nonstopmode {filename} | grep "LaTeX Error: File"'
     a = s.run([cmd], shell=True, stdout=s.PIPE, stderr=s.STDOUT)
     result = a.stdout.decode().split('\n')[0]
     if result == '':
         return None
-    missing_pkg = re.findall("`(.*)'", result)[0]
-    return missing_pkg
+    return re.findall("`(.*)'", result)[0]
 
 def find(pkg):
-    cmd = f'tlmgr search --global --file "/{pkg}"'
-    a = s.run([cmd], shell=True, stdout=s.PIPE, stderr=s.STDOUT)
+    cmd = f'tlmgr search --global --file "/{pkg}"'.split()
+    a = s.run(cmd, stdout=s.PIPE, stderr=s.STDOUT)
     result = a.stdout.decode().split('\n')[-3][:-1]
     if len(result.split()) == 1:
         return result
-    raise
+    print(f'Unable to find {pkg}')
+    print(a.stdout, file=sys.stderr)
+    exit(1)
 
 def install(pkg):
-    cmd = f'tlmgr install {pkg}'
-    s.run([cmd], shell=True)
+    s.run(f'tlmgr install {pkg}'.split())
 
 def update():
-    s.run(['tlmgr update --self --all'], shell=True)
-    s.run(['tlmgr path add'], shell=True)
-    s.run(['fmtutil-sys --all'], shell=True)
-
-def fix(pkg):
-    install(find(pkg))
-    update()
+    s.run('tlmgr update --self --all'.split())
+    s.run('tlmgr path add'.split())
+    s.run('fmtutil-sys --all'.split())
 
 if __name__ == "__main__":
-    compile_while(sys.argv[1])
+    import argparse as a
+    import textwrap
+    p = a.ArgumentParser(
+        prog='auto_tinytex',
+        formatter_class=a.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        Installs missing packages until your LaTeX document compiles.
+        This allows for installing only what the document requires,
+        and saves space compared to installing texlive-full/extra etc.
+
+        This utility wraps tinytex, a minimal texlive distribution.
+        See also: https://github.com/yihui/tinytex'''))
+    p.add_argument('texfile', help='LaTeX document to compile')
+    args = p.parse_args()
+    compile_until_no_errors(args.texfile)
     update()
